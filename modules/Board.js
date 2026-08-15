ff574d35b1aba9138c86f6be74f1cd7efb350b46
yasser kaci <kaciyasserabdeldjalil@gmail.com>
2026-08-15 21:38:08 +0100

    legal-moves

--- modules/Board.js
+++ modules/Board.js
@@ -10,16 +10,126 @@ export default function board(){
         ['WR','WKN','WB','WQ','WK','WB','WKN','WR'],
     ];
 
+    const inBound = (r, c) => r > -1 && r < 8 && c > -1 && c < 8 
 
+
+    const slide = (row, col, directions, color) =>{
+        let moves = []
+        for(const [dr, dc] of directions){
+            let r = row + dr
+            let c = col + dc
+            while(inBound(r,c)){
+                const target = this.board[r][c]
+                console.log(target[0])
+                if(target === ''){
+                    moves.push([r, c])
+                }else{
+                    if(target[0] !== color){
+                        moves.push([r, c])
+                        break
+                    }else{break}
+                }
+                r += dr
+                c += dc
+
+            } 
+        }
+        return moves
+    }
+
+    this.getLegalMoves = function(first){
+        const [r, c] = first
+        const node = this.board[r][c]
+        if (node === '') {return}
+        const color = node[0]
+        const type = node.slice(1)
+        let moves = []
+        if(type === 'P'){
+            const dir = color ==='W'? -1 : 1 
+            console.log(color, type, [r, c])
+            const double = (color === 'W' && r === 6) || (color === 'B' && r === 1)
+            if (double && this.board[r + dir * 2][c] === ''){
+                moves.push([r+dir*2, c])
+            }
+            if(inBound(r + dir, c) && this.board[r + dir][c] === ''){
+                console.log('nigaa')
+                moves.push([r+dir, c])
+            }
+        }
+        if(type ==='R'){
+            const dir = [[1,0],[-1,0],[0,1],[0,-1]]
+            moves.push(...slide(r, c,dir, color))
+        }
+        if(type ==='B'){
+            const dir = [[1,1],[-1,1],[1,-1],[-1,-1]]
+            moves.push(...slide(r, c,dir, color))
+        }
+        if(type ==='Q'){
+            const dir = [[1,1],[-1,1],[1,-1],[-1,-1],[1,0],[-1,0],[0,1],[0,-1]]
+            moves.push(...slide(r, c,dir, color))
+        }
+
+        if(type ==='KN'){
+            const dir = [[2,1],[-2,1],[2,-1],[-2,-1],[1,2],[-1,2],[1,-2],[-1,-2]]
+            for(const [dr, dc] of dir){
+            let r = first[0] + dr
+            let c = first[1] + dc
+            if(inBound(r,c)){
+                const target = this.board[r][c]
+                console.log(target[0])
+                if(target === ''){
+                    moves.push([r, c])
+                }else{
+                    if(target[0] !== color){
+                        moves.push([r, c])
+                    }
+                }
+            } 
+        }
+        }
+
+        if(type === 'K'){            
+            const dir = [[1,1],[-1,1],[1,-1],[-1,-1],[1,0],[-1,0],[0,1],[0,-1]]
+            for(const [dr, dc] of dir){
+                let r = first[0] + dr
+                let c = first[1] + dc
+                if(inBound(r , c) && this.board[r][c] === ''){
+                    console.log('nigaa')
+                    moves.push([r, c])
+                }
+        }
+        }
+
+
+        console.log(moves)
+        return moves
+    }
+
+
+    this.move = function(start,end){
+        console.log(start)
+        const type = this.board[start[0]][ start[1]]
+        console.log('type:', type)
+        const node = document.querySelector(`[data-row='${start[0]}'][data-col='${start[1]}']`)
+        console.log(node)
+        this.board[start[0]][ start[1]] = ''
+        console.log(this.board[start[0]][ start[1]])
+        
+        
+        this.board[end[0]][ end[1]] = type
+        this.draw()
+
+    }
 
 
     this.draw = function(){
-        for(let j = 0; j< this.board.length; j++){
-            for (let i = 0; i< this.board[j].length ; i++){
-                const img = document.createElement('img')
-                if(this.board[j][i] !== ''){
-                img.src = `./styles/images/${this.board[j][i]}.svg`
+        for(let j = 0; j < this.board.length; j++){
+            for (let i = 0; i< this.board.length ; i++){
                 const node = document.querySelector(`[data-row='${j}'][data-col='${i}']`)
+                node.innerHTML = ''
+                if(this.board[j][i] !== ''){
+                    const img = document.createElement('img')
+                img.src = `./styles/images/${this.board[j][i]}.svg`
                 node.appendChild(img)
                 }
             }
--- script.js
+++ script.js
@@ -1,38 +1,94 @@
-import boardO from "./modules/Board.js"
+import boardO from "./modules/Board.js";
 
-const board = document.querySelector('.board')
-const squareWidth = 70
-const squareAmount = 8
+const board = document.querySelector('.board');
+const squareWidth = 70;
+const squareAmount = 8;
 
-console.log(`${squareWidth * squareAmount}px`, board)
+let turn = 'W';
 
-board.style.width = `${squareWidth * squareAmount}px`
-board.style.height = `${squareWidth * squareAmount}px`
+board.style.width = `${squareWidth * squareAmount}px`;
+board.style.height = `${squareWidth * squareAmount}px`;
 
-
-for (let i = 0; i < squareAmount  ; i++){
-    for (let j = 0; j < squareAmount; j++ ){
-        const node = document.createElement('div')
-            node.style.width = `${squareWidth}px`
-            node.style.height = `${squareWidth}px`
-            node.classList.add('node')
-            node.dataset.col = j
-            node.dataset.row = i
-        if((i + j) % 2 === 0 ){
-            node.style.background = '#7B61FF'
-        }else{
-            node.style.background = '#E8EDF9'
+// 1. Create the board grid in the DOM
+for (let r = 0; r < squareAmount; r++) {
+    for (let c = 0; c < squareAmount; c++) {
+        const node = document.createElement('div');
+        node.style.width = `${squareWidth}px`;
+        node.style.height = `${squareWidth}px`;
+        node.classList.add('node');
+        node.dataset.row = r;
+        node.dataset.col = c;
+        
+        if ((r + c) % 2 === 0) {
+            node.style.background = '#7B61FF';
+        } else {
+            node.style.background = '#E8EDF9';
         }
 
-        board.appendChild(node)
+        board.appendChild(node);
     }
 }
 
-const Boardd = new boardO()
+const Boardd = new boardO();
 
-Boardd.draw()
+let start = null;
+let legalMoves = [];
+const isLegal = (row, col) => legalMoves.some(([r, c]) => r === row && c === col);
 
+const clearHilight = () => {
+    document.querySelectorAll('.legal-move').forEach((el) => {
+        el.classList.remove('legal-move');
+    });
+};
 
+// Helper function to handle selecting a piece and highlighting its moves
+const selectPiece = (r, c) => {
+    // Check if the square is empty or belongs to the opponent
+    if (Boardd.board[r][c] === '' || Boardd.board[r][c][0] !== turn) {
+        start = null;
+        clearHilight();
+        return false;
+    }
 
+    start = [r, c];
+    legalMoves = Boardd.getLegalMoves(start);
+    clearHilight();
+    
+    legalMoves.forEach(([mr, mc]) => {
+        const current = document.querySelector(`[data-row='${mr}'][data-col='${mc}']`);
+        if (current) current.classList.add('legal-move');
+    });
+    
+    return true;
+};
 
+// 2. Attach click listeners to all nodes using correct row/col attributes
+document.querySelectorAll('.node').forEach((node) => {
+    node.addEventListener('click', (e) => {
+        const r = Number(e.currentTarget.dataset.row);
+        const c = Number(e.currentTarget.dataset.col);
 
+        if (start === null) {
+            // No piece selected yet; try selecting this one
+            selectPiece(r, c);
+        } else {
+            // Piece already selected; check if click is a legal move destination
+            if (isLegal(r, c)) {
+                Boardd.move(start, [r, c]);
+                turn = turn === 'W' ? 'B' : 'W';
+                start = null;
+                clearHilight();
+            } else {
+                // Clicked an illegal destination; treat it as trying to select a new piece instead
+                const selectedSuccessfully = selectPiece(r, c);
+                if (!selectedSuccessfully) {
+                    start = null;
+                    clearHilight();
+                }
+            }
+        }
+        console.log("Current start:", start, "Turn:", turn);
+    });
+});
+
+Boardd.draw();
